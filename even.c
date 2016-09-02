3c97b2dfac80a715b01f06799763773f7c4d6cf8
Swethasuvi07 <swethasuvi07@gmail.com>
2016-09-02 14:03:37 +0530

    Update even.c

--- even.c
+++ even.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 void main()
 {
-int i,num;
+int num;
 printf("Enter the number");
 scanf("%d",&num);
 if(num%2==0)
 {
 printf("number is even");
+}
 else
+{
 printf("number is odd");
 }
 }
